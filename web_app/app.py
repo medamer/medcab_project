98f8373bbc2ce0633ae1607ac7b2b22fae1bd55b
Mohamed Edamer <edamer.mo@gmail.com>
2021-01-21 18:30:20 -0800

    in progress

--- web_app/app.py
+++ web_app/app.py
@@ -1,10 +1,29 @@
 import pandas as pd
-from web_app.model import my_prediction
+from model import my_prediction
 from flask import Flask, render_template, request
+from dotenv import load_dotenv
+import psycopg2
+import os
+from psycopg2.extras import execute_values
+
+
+# Load dotenv
+load_dotenv()
+
+# Get the credentials from .env file:
+DB_USER = os.getenv("USER")
+DB_NAME = os.getenv("USER")
+DB_PASSWORD = os.getenv("PASSWORD")
+DB_HOST = os.getenv("SERVER")
+
+# Connect to the Database:
+connection = psycopg2.connect(dbname=DB_USER, user=DB_NAME, password=DB_PASSWORD, host=DB_HOST)
+cursor = connection.cursor()
+
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
@@ -12,7 +31,18 @@ def index():
 def prediction():
     user_input = request.form['comment']
     pred = my_prediction(user_input)
-    return render_template('prediction.html', data=pred)
+    qeury = f"""
+            select strain_name as Name, strain_type as Type, strain_rating as Rating, 
+	               strain_effects as Effects, strain_description as Description, 
+	               strain_flavors as Flavors from medcab
+                WHERE
+                    strain_id in({pred[0]},{pred[1]},{pred[2]}, {pred[3]}, {pred[4]})
+            order by strain_rating desc;
+            """
+    cursor.execute(query)
+    myresult = cursor.fetchall()
+
+    return render_template('prediction.html', predicted=myresult)
 
 if __name__ == '__main__':
     app.debug = True
--- web_app/insertdb.py
+++ web_app/insertdb.py
@@ -22,39 +22,39 @@ connection = psycopg2.connect(dbname=DB_USER, user=DB_NAME, password=DB_PASSWORD
 cursor = connection.cursor()
 print('CONNECTED')
 
-# Create our table:
-query = """
-        CREATE TABLE IF NOT EXISTS medcab (
-            strain_id SERIAL PRIMARY KEY,
-            strain_name varchar(1000),
-            strain_type varchar(100),
-            strain_rating float,
-            strain_effects varchar(20000),
-            strain_description varchar(20000),
-            strain_flavors varchar(1000),
-            strain_nearest varchar(1000)
-        ) """
-cursor.execute(query)
-connection.commit()
-print("Table Created")
+# # Create our table:
+# query = """
+#         CREATE TABLE IF NOT EXISTS medcab (
+#             strain_id SERIAL PRIMARY KEY,
+#             strain_name varchar(1000),
+#             strain_type varchar(100),
+#             strain_rating float,
+#             strain_effects varchar(20000),
+#             strain_description varchar(20000),
+#             strain_flavors varchar(1000),
+#             strain_nearest varchar(1000)
+#         ) """
+# cursor.execute(query)
+# connection.commit()
+# print("Table Created")
 
-# Read the csv file:
-file_path =  './data/cannabis_new.csv' #os.path.join(os.path.dirname(__file__), "../cannabis_new.csv")
-df = pd.read_csv(file_path)
-psycopg2.extensions.register_adapter(np.int64, psycopg2._psycopg.AsIs)
+# # Read the csv file:
+# file_path =  './data/cannabis_new.csv' #os.path.join(os.path.dirname(__file__), "../cannabis_new.csv")
+# df = pd.read_csv(file_path)
+# psycopg2.extensions.register_adapter(np.int64, psycopg2._psycopg.AsIs)
 
-# Load the csv file to the database:
-list_of_tuples = list(df.to_records(index=False))
+# # Load the csv file to the database:
+# list_of_tuples = list(df.to_records(index=False))
 
-insert_query = """
-                INSERT INTO medcab(
-                    strain_id, strain_name, strain_type, strain_rating, strain_effects, strain_description,
-                    strain_flavors, strain_nearest
-                ) VALUES %s ;
-                """
+# insert_query = """
+#                 INSERT INTO medcab(
+#                     strain_id, strain_name, strain_type, strain_rating, strain_effects, strain_description,
+#                     strain_flavors, strain_nearest
+#                 ) VALUES %s ;
+#                 """
 
-execute_values(cursor, insert_query, list_of_tuples)
-connection.commit()
+# execute_values(cursor, insert_query, list_of_tuples)
+# connection.commit()
 
 
 # Check the table:
