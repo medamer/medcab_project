74efe261aaeabaa7d874cc521c75a20f5e2b763a
Mohamed Edamer <edamer.mo@gmail.com>
2021-01-28 15:34:23 -0800

    updated app file

--- web_app/app.py
+++ web_app/app.py
@@ -58,5 +58,5 @@ def prediction():
                                               pred3=pred3, pred4=pred4, pred5=pred5)
 
 if __name__ == '__main__':
-    app.debug = True
+    #app.debug = True
     app.run()
